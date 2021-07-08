690578ccd04c98ee0ac2b49d925a09b45154e0c4
Watsapon Kampong <ty.watsapon@bitkub.com>
2021-07-08 15:10:34 +0700

    chore: init commit

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+	extends: [
+		'airbnb',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jest/recommended',
+		'plugin:prettier/recommended',
+	],
+	plugins: ['react', '@typescript-eslint', 'jest'],
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+	},
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+		'react/prop-types': 'off',
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	singleQuote: true,
+	useTabs: true,
+	tabWidth: 4,
+	semi: false,
+	jsxSingleQuote: true,
+	arrowParens: 'avoid',
+	printWidth: 120
+}
