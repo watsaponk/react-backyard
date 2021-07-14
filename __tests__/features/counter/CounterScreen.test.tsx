26278ef23654bc30f47352de706d8f42762d6ef9
Watsapon Kampong <ty.watsapon@bitkub.com>
2021-07-14 15:47:47 +0700

    chore: update test config

--- __tests__/features/counter/CounterScreen.test.tsx
+++ __tests__/features/counter/CounterScreen.test.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { render, RenderAPI, fireEvent } from '@testing-library/react-native'
 import { Provider } from 'react-redux'
+import { configureStore, Store } from '@reduxjs/toolkit'
 import CounterScreen from '../../../src/features/counter/CounterScreen'
-import { increase, decrease } from '../../../src/features/counter/redux/CounterReducer'
-import { store } from '../../../src/shared/redux/Store'
+import CounterReducer, { increase, decrease } from '../../../src/features/counter/redux/CounterReducer'
 
-const renderScreen = (): RenderAPI => {
-	store.dispatch = jest.fn()
-
+const renderScreen = (store: Store): RenderAPI => {
 	return render(
 		<Provider store={store}>
 			<CounterScreen />
@@ -15,29 +13,49 @@ const renderScreen = (): RenderAPI => {
 	)
 }
 
+const createStore = (): Store => {
+	return configureStore({
+		reducer: {
+			counter: CounterReducer,
+		},
+	})
+}
+
 describe('CounterScreen', () => {
 	test('When render on start', () => {
-		const component = renderScreen()
+		const component = renderScreen(createStore())
 		expect(component.toJSON()).toMatchSnapshot()
 	})
 
 	test('When press PLUS button should dispatch increase action', () => {
-		const { getByTestId } = renderScreen()
+		const store = createStore()
+		const spyDispatch = jest.spyOn(store, 'dispatch')
+		const { getByTestId } = renderScreen(store)
 		const buttonPlus = getByTestId('button_plus')
+		const textCount = getByTestId('text_count')
 
 		fireEvent.press(buttonPlus)
 
-		expect(store.dispatch).toHaveBeenCalledTimes(1)
-		expect(store.dispatch).toHaveBeenCalledWith(increase())
+		expect(spyDispatch).toHaveBeenCalledTimes(1)
+		expect(spyDispatch).toHaveBeenCalledWith(increase())
+		expect(textCount).toHaveTextContent(/^1$/)
+		expect(store.getState().counter.value).toEqual(1)
+		spyDispatch.mockClear()
 	})
 
 	test('When press MINUS button should dispatch decrease action', () => {
-		const { getByTestId } = renderScreen()
+		const store = createStore()
+		const spyDispatch = jest.spyOn(store, 'dispatch')
+		const { getByTestId } = renderScreen(store)
 		const buttonMinus = getByTestId('button_minus')
+		const textCount = getByTestId('text_count')
 
 		fireEvent.press(buttonMinus)
 
-		expect(store.dispatch).toHaveBeenCalledTimes(1)
-		expect(store.dispatch).toHaveBeenCalledWith(decrease())
+		expect(spyDispatch).toHaveBeenCalledTimes(1)
+		expect(spyDispatch).toHaveBeenCalledWith(decrease())
+		expect(textCount).toHaveTextContent(/^-1$/)
+		expect(store.getState().counter.value).toEqual(-1)
+		spyDispatch.mockClear()
 	})
 })
