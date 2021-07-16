80fde04d20c1a0d2cf4d9366526d67bad5dfb0e6
Watsapon Kampong <ty.watsapon@bitkub.com>
2021-07-16 17:04:31 +0700

    refactor: remove useless code

--- __tests__/features/counter/service/GetMagicNumber.test.ts
+++ __tests__/features/counter/service/GetMagicNumber.test.ts
@@ -1,9 +1,5 @@
 import GetMagicNumber from '../../../../src/features/service/GetMagicNumber'
 
-function flushPromises() {
-	return new Promise(resolve => process.nextTick(resolve))
-}
-
 function spyMathRandom(value: number) {
 	jest.spyOn(global.Math, 'random').mockReturnValue(value)
 }
@@ -15,22 +11,20 @@ afterEach(() => {
 	jest.spyOn(global.Math, 'random').mockRestore()
 })
 
-test('Should resolve with positive number lesser than 5', async () => {
+test('Should resolve with positive number lesser than 5', () => {
 	spyMathRandom(0.6)
 
 	const sut = GetMagicNumber().then(result => expect(result).toEqual(3))
 
 	jest.advanceTimersByTime(1000)
-	await flushPromises()
 	return sut
 })
 
-test('Should resolve with positive number greater than -5', async () => {
+test('Should resolve with positive number greater than -5', () => {
 	spyMathRandom(0.4)
 
 	const sut = GetMagicNumber().then(result => expect(result).toEqual(-2))
 
 	jest.advanceTimersByTime(1000)
-	await flushPromises()
 	return sut
 })
