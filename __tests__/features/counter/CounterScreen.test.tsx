8a0fc348a21b04556922e72491c07c10abf2fe07
Watsapon Kampong <ty.watsapon@bitkub.com>
2021-07-22 14:57:36 +0700

    test: rollback CounterScreen test and remove Counter test

---
+++ __tests__/features/counter/CounterScreen.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { fireEvent } from '@testing-library/react-native'
+import CounterScreen from '../../../src/features/counter/CounterScreen'
+import { increase, decrease } from '../../../src/features/counter/CounterRedux'
+import buildStore from '../../../src/shared/redux/Store'
+import { renderScreenWithStore, spyOnStoreDispatch } from '../../TestUtil'
+
+test('When press PLUS button should dispatch increase action', () => {
+	const store = buildStore()
+	const spyDispatch = spyOnStoreDispatch(store)
+	const { getByTestId } = renderScreenWithStore(store, <CounterScreen />)
+	const buttonPlus = getByTestId('button_plus')
+	const textCount = getByTestId('text_count')
+
+	fireEvent.press(buttonPlus)
+
+	expect(spyDispatch).toHaveBeenCalledWith(increase())
+	expect(textCount).toHaveTextContent(/^1$/)
+	expect(store.getState().counter.value).toEqual(1)
+	spyDispatch.mockClear()
+})
+
+test('When press MINUS button should dispatch decrease action', () => {
+	const store = buildStore()
+	const spyDispatch = spyOnStoreDispatch(store)
+	const { getByTestId } = renderScreenWithStore(store, <CounterScreen />)
+	const buttonMinus = getByTestId('button_minus')
+	const textCount = getByTestId('text_count')
+
+	fireEvent.press(buttonMinus)
+
+	expect(spyDispatch).toHaveBeenCalledWith(decrease())
+	expect(textCount).toHaveTextContent(/^-1$/)
+	expect(store.getState().counter.value).toEqual(-1)
+	spyDispatch.mockClear()
+})
